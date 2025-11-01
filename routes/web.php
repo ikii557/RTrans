63095560d959d5d2cc71ab6d46cd57b43fcc5c03
ikii557 <rifkihadidanwar@gmail.com>
2025-11-02 00:07:26 +0700

    penambahan pages dan potong template

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+
+
+Route::get('/register', function () {
+    return view('pages.auth.register');
+});
+Route::get('/login', function () {
+    return view('pages.auth.login');
+});
+
+
+Route::get('/index', function () {
+    return view('pages.index');
+});
+Route::get('/detail', function () {
+    return view('pages.index');
+});
+Route::get('/summaryday', function () {
+    return view('pages.index');
+});
+Route::get('/summarymonth', function () {
+    return view('pages.index');
+});
+Route::get('/summaryyear', function () {
+    return view('pages.index');
+});
+Route::get('/resto', function () {
+    return view('pages.index');
+});
+Route::get('/hotel', function () {
+    return view('pages.hotel.index_hotel');
 });
